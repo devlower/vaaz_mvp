631e42459c77ce79afe950ff1ccfe4e5ccabf6aa
devlower <71558232+devlower@users.noreply.github.com>
2022-11-06 19:58:31 -0300

    Login Screen

--- app/src/main/java/com/devlower/vaaz/LoginActivity.kt
+++ app/src/main/java/com/devlower/vaaz/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.devlower.vaaz
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devlower.vaaz.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var binding: ActivityLoginBinding? = null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.tvSignIn?.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
