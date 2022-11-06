bb7626613d409d205f53f36ebd484823c150fd58
devlower <71558232+devlower@users.noreply.github.com>
2022-11-06 14:27:38 -0300

    Splash screen

---
+++ app/src/main/java/com/devlower/vaaz/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.devlower.vaaz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+        }, 3000)
+    }
+}
