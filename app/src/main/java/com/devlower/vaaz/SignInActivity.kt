724a1e776ca7d5aa3689604f5511305849269150
devlower <71558232+devlower@users.noreply.github.com>
2022-11-07 09:53:53 -0300

    add navigation functions on sign in screen

--- app/src/main/java/com/devlower/vaaz/SignInActivity.kt
+++ app/src/main/java/com/devlower/vaaz/SignInActivity.kt
@@ -1,11 +1,27 @@
 package com.devlower.vaaz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devlower.vaaz.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private var binding: ActivitySignInBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.icBackArrow?.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
