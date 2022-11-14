2a2c0536a78fa27f526aae2db2e25e5dd1c976c1
devlower <71558232+devlower@users.noreply.github.com>
2022-11-14 12:07:33 -0300

    Home screen layout fix - 1st navigation

--- app/src/main/java/com/devlower/vaaz/HomeActivity.kt
+++ app/src/main/java/com/devlower/vaaz/HomeActivity.kt
@@ -1,11 +1,30 @@
 package com.devlower.vaaz
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devlower.vaaz.databinding.ActivityHomeBinding
+import com.devlower.vaaz.databinding.ActivityLoginBinding
 
 class HomeActivity : AppCompatActivity() {
+    private var binding: ActivityHomeBinding? = null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.icBackArrow?.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
