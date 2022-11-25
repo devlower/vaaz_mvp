f9365e282b59a12e725d85fe222b74636f98306a
devlower <71558232+devlower@users.noreply.github.com>
2022-11-25 09:42:27 -0300

    PlantStatusActivity layout

---
+++ app/src/main/java/com/devlower/vaaz/PlantStatusActivity.kt
@@ -0,0 +1,11 @@
+package com.devlower.vaaz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlantStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_status)
+    }
+}
